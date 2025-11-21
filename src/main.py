99e62452343976eb9d71cbdfe16e2242f13ab876
vrdoshi <vruj.doshi@gmail.com>
2025-11-21 15:32:37 +0000

    added timer command and fixed secret handling

--- src/main.py
+++ src/main.py
@@ -1,32 +1,54 @@
 import discord
+import os
+import asyncio
+from dotenv import load_dotenv
+from discord.ext import commands
 
-from src.config import bot_token
 
+load_dotenv()
+bot_token = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'we have logged in as {client.user}')
-
-
-@client.event
-async def on_message(message):
-    print(f"Message received: '{message.content}' from {message.author}")
-
-    if message.author == client.user:
-        print("Message is from bot, ignoring")
-        return
-
-    if message.content.lower().startswith('hello'):
-        print("Sending Hello!! response")
-        await message.channel.send('Hello!!')
-    else:
-        print(f"Message didn't match: '{message.content.lower()}'")
+current_timers = {}
+@bot.command('pomo')
+async def pomo(ctx, action: str, length: int):
+    if action == 'start':
+        # check existing timer
+        if ctx.author in current_timers.keys():
+            ctx.send(f'you already have an existing timer, {ctx.author}.')
+        else:
+            # start timer
+            await ctx.send('starting timer... 🍅')
+            current_timers[ctx.author] =  length  # timer length will be parameterised
+            await asyncio.sleep(length)
+            await ctx.send('timer is up!')
 
 
-client.run(bot_token)
+
+
+# @client.event
+# async def on_ready():
+#     print(f'we have logged in as {client.user}')
+#
+#
+# @client.event
+# async def on_message(message):
+#     print(f"Message received: '{message.content}' from {message.author}")
+#
+#     if message.author == client.user:
+#         print("Message is from bot, ignoring")
+#         return
+#
+#     if message.content.lower().startswith('hello'):
+#         print("Sending Hello!! response")
+#         await message.channel.send('Hello!!')
+#     else:
+#         print(f"Message didn't match: '{message.content.lower()}'")
+
+
+bot.run(bot_token)
 
